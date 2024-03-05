6e977c55ecbb6153216a94a26533f5851feb0f79
Anton Klock <klock.anton@gmail.com>
2024-03-05 14:11:06 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import svelteLogo from "./assets/svelte.svg";
+	import viteLogo from "/vite.svg";
+	import Counter from "./lib/Counter.svelte";
+	import electronLogo from "./assets/electron.svg";
+	import tailwindLogo from "./assets/tailwind.svg";
+
+	const nodeVersion = api.node();
+	const chromeVersion = api.chrome();
+	const electronVersion = api.electron();
+</script>
+
+<main
+	class="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center"
+>
+	<div class="flex flex-col">
+		<h1>Create dynamic Init videos</h1>
+		<button class="btn btn-primary">Generate</button>
+	</div>
+</main>
